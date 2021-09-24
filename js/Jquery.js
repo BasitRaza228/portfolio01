0533e70cd869e8cc683ba6c2d8ab6320ff207b9e
Syed Basit Raza <basitraza228@gmail.com>
2021-09-24 11:53:08 +0500

    minor adjustments

--- js/Jquery.js
+++ js/Jquery.js
@@ -6,9 +6,18 @@ $(document).ready(function () {
         $(document).click(function () {
             // if($(".navbar-collapse").hasClass("in")){
               $('.navbar-collapse').collapse('hide');
+              $("#MainPage-carousel").carousel({interval: 3000});
+
+        // Enable Carousel Controls
+        $(".carousel-control-prev").click(function () {
+            $("#MainPage-carousel").carousel("prev");
+        });
+        $(".carousel-control-next").click(function () {
+            $("#MainPage-carousel").carousel("next");
+        });
             // }
          });
-    $("#MainPage-carousel").carousel({interval: 4000});
+    $("#MainPage-carousel").carousel({interval: 3000});
 
         // Enable Carousel Controls
         $(".carousel-control-prev").click(function () {
